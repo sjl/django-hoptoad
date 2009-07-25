292400c1597f49d919b055748c46c2994948963f
Steve Losh <steve@stevelosh.com>
2009-07-25 18:59:03 -0400

    Rename a lot of parameters to make things more readable.

--- hoptoad/middleware.py
+++ hoptoad/middleware.py
@@ -5,64 +5,65 @@ from django.core.exceptions import MiddlewareNotUsed
 from django.views.debug import get_safe_settings
 from django.conf import settings
 
-def _parse_environment(req):
+def _parse_environment(request):
     env = dict( (str(k), str(v)) for (k, v) in get_safe_settings().items() )
-    env.update(dict( (str(k), str(v)) for (k, v) in req.META.items() ))
-    env['REQUEST_URI'] = req.build_absolute_uri()
+    env.update( dict( (str(k), str(v)) for (k, v) in request.META.items() ) )
+    
+    env['REQUEST_URI'] = request.build_absolute_uri()
+    
     return env
 
-def _parse_traceback(tb):
-    tb = [ "%s:%d:in `%s'" % (filename, lineno, funcname) 
-                  for filename, lineno, funcname, _
-                  in traceback.extract_tb(tb) ]
-    tb.reverse()
-    return tb
+def _parse_traceback(trace):
+    p_traceback = [ "%s:%d:in `%s'" % (filename, lineno, funcname) 
+                    for filename, lineno, funcname, _
+                    in traceback.extract_tb(trace) ]
+    p_traceback.reverse()
+    
+    return p_traceback
 
-def _parse_message(exc, exc_class):
-    return '%s: %s' % (exc_class.__name__, str(exc))
+def _parse_message(exc):
+    return '%s: %s' % (exc.__class__.__name__, str(exc))
 
-def _parse_request(req):
-    request_get = dict( (str(k), str(v)) for (k, v) in req.GET.items() )
-    request_post = dict( (str(k), str(v)) for (k, v) in req.POST.items() )
+def _parse_request(request):
+    request_get = dict( (str(k), str(v)) for (k, v) in request.GET.items() )
+    request_post = dict( (str(k), str(v)) for (k, v) in request.POST.items() )
     return request_post if request_post else request_get
 
 def _parse_session(session):
     return dict( (str(k), str(v)) for (k, v) in session.items() )
 
 
-def _generate_payload(req, exc=None, excc=None, tb=None, mess=None, err_class=None):
+def _generate_payload(request, exc=None, trace=None, message=None, error_class=None):
     """Generate a YAML payload for a Hoptoad notification.
     
     Parameters:
-    req -- A Django HTTPRequest.  This is required.
+    request -- A Django HTTPRequest.  This is required.
     
     Keyword parameters:
     exc -- A Python Exception object.  If this is not given the 
            mess parameter must be.
-    excc -- A Python class object representing the exception class.  If
-            this is not given the err_class parameter must be.
-    tb -- A Python Traceback object.  This is not required.
-    mess -- A string representing the error message.  If this is not
-            given, the exc parameter must be.
-    err_class -- A string representing the error class.  If this is not
-                 given the excc parameter must be.
+    trace -- A Python Traceback object.  This is not required.
+    message -- A string representing the error message.  If this is not
+               given, the exc parameter must be.
+    error_class -- A string representing the error class.  If this is not
+                   given the excc parameter must be.
     """
-    message = mess if mess else _parse_message(exc, excc)
-    error_class = err_class if err_class else excc.__name__
-    traceback = _parse_traceback(tb) if tb else []
-    environment = _parse_environment(req)
-    request = _parse_request(req)
-    session = _parse_session(req.session)
-    
+    p_message = message if message else _parse_message(exc)
+    p_error_class = error_class if error_class else exc.__class__.__name__
+    p_traceback = _parse_traceback(trace) if trace else []
+    p_environment = _parse_environment(request)
+    p_request = _parse_request(request)
+    p_session = _parse_session(request.session)
+    print p_error_class
     return yaml.dump({ 'notice': {
         'api_key':       settings.HOPTOAD_API_KEY,
-        'error_class':   error_class,
-        'error_message': message,
-        'backtrace':     traceback,
-        'request':       { 'url': req.build_absolute_uri(),
-                           'params': request },
-        'session':       { 'key': '', 'data': session },
-        'environment':   environment,
+        'error_class':   p_error_class,
+        'error_message': p_message,
+        'backtrace':     p_traceback,
+        'request':       { 'url': request.build_absolute_uri(),
+                           'params': p_request },
+        'session':       { 'key': '', 'data': p_session },
+        'environment':   p_environment,
     }}, default_flow_style=False)
 
 def _ride_the_toad(payload, timeout):
@@ -131,7 +132,7 @@ class HoptoadNotifierMiddleware(object):
         """
         excc, _, tb = sys.exc_info()
         
-        payload = _generate_payload(request, exc, excc, tb)
+        payload = _generate_payload(request, exc, tb)
         _ride_the_toad(payload, self.timeout)
         
         return None
