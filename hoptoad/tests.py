4633ddcffd91486edf91b0b35d7721c1e2c5d31f
Steve Losh <steve@stevelosh.com>
2009-08-28 21:54:14 -0400

    Add some simple docstrings for the tests.

--- hoptoad/tests.py
+++ hoptoad/tests.py
@@ -6,12 +6,14 @@ class BasicTests(TestCase):
     """Basic tests like setup and connectivity."""
     
     def test_api_key_present(self):
+        """Test to make sure an API key is present."""
         self.assertTrue('HOPTOAD_API_KEY' in settings.get_all_members(),
             msg='The HOPTOAD_API_KEY setting is not present.')
         self.assertTrue(settings.HOPTOAD_API_KEY,
             msg='The HOPTOAD_API_KEY setting is blank.')
     
     def test_hoptoad_connectivity(self):
+        """Test to make sure hoptoadapp.com can be reached at all."""
         try:
             ht = urllib2.urlopen('http://hoptoadapp.com/')
         except urllib2.HTTPError:
