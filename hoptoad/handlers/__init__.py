ab462540a3470dc75ae74cabc5b54068360fe092
mahmoudimus <none@none>
2010-01-20 02:58:33 -0800

    Implement custom handlers and update the documentations

--- hoptoad/handlers/__init__.py
+++ hoptoad/handlers/__init__.py
@@ -2,6 +2,9 @@
 various different protocols.
 """
 import logging
+import os
+import imp
+import pprint
 
 from hoptoad import get_hoptoad_settings
 from hoptoad.handlers.threaded import ThreadedNotifier
@@ -19,3 +22,17 @@ def get_handler(*args, **kwargs):
         return ThreadedNotifier(threads , *args, **kwargs)
     elif handler.lower() == 'blocking':
         return BlockingNotifier(*args, **kwargs)
+    else:
+        _class_module = hoptoad_settings.get('HOPTOAD_HANDLER_CLASS', None)
+        if not _class_module:
+            # not defined, abort setting up hoptoad, skip it.
+            raise MiddlewareNotUsed
+        # module name that we should import from
+        _module_name = os.path.splitext(os.path.basename(handler))[0]
+        # load the module!
+        m = imp.load_module(_module_name,
+                            *imp.find_module(_module_name,
+                                             [os.path.dirname(handler)]))
+
+        # instantiate the class
+        return getattr(m, _class_module)(*args, **kwargs)
