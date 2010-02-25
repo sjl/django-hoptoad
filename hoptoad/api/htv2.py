ff3fbd54d125d187538349cf5454e95d0ac94d7f
bmjames <bmjames@gmail.com>
2010-02-25 21:33:02 +0000

    Use get method instead of try/catch for default environment name
    Suggested by Mahmoud Abdelkader

--- hoptoad/api/htv2.py
+++ hoptoad/api/htv2.py
@@ -184,10 +184,7 @@ def generate_payload(request_tuple):
     #          dynamically. It would simply be the responsibility of
     #          settings.py to do it, rather than the hoptoad middleware.
 
-    try:
-        envname_text = hoptoad_settings['HOPTOAD_ENV_NAME']
-    except KeyError:
-        envname_text = 'Unknown'
+    envname_text = hoptoad_settings.get('HOPTOAD_ENV_NAME', 'Unknown')
     envname_data = xdoc.createTextNode(envname_text)
     envname.appendChild(envname_data)
     serverenv.appendChild(envname)
